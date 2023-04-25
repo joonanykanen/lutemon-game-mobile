1440b9ce47f3fdb5663ce23c2b1b4188d3e1c5bc
Joona Nykänen <joona.nykanen@icloud.com>
2023-04-25 17:31:09 +0300

    feat: Added MainActivity features

--- app/src/main/java/com/example/lutemon_game_mobile/MainActivity.java
+++ app/src/main/java/com/example/lutemon_game_mobile/MainActivity.java
@@ -1,14 +1,56 @@
 package com.example.lutemon_game_mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button homeButton;
+    private Button trainingButton;
+    private Button battleButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        homeButton = findViewById(R.id.homeButton);
+        trainingButton = findViewById(R.id.trainingButton);
+        battleButton = findViewById(R.id.battleButton);
+
+        setFragment(new HomeFragment());
+
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setFragment(new HomeFragment());
+            }
+        });
+
+        trainingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setFragment(new TrainingFragment());
+            }
+        });
+
+        battleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setFragment(new BattleFragment());
+            }
+        });
+    }
+
+    private void setFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, fragment);
+        fragmentTransaction.commit();
     }
 }
